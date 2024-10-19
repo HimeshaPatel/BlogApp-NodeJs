cc1e70865c528c47c608390695f643160a44d99b
himesha <himesha.patel7@gmail.com>
2024-10-20 05:14:05 +0530

    "blog-start"

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose')
 const app = express();
 const cookieParser = require('cookie-parser')   
 
+const Blog = require('./models/blog');
+
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
 const { checkForAuthenticationCookie } = require('./middlewares/aunthentication');
 
 app.use(express.json());
@@ -20,20 +24,18 @@ app.set("views", path.resolve("./views"))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve('./public')))
 
 
-// app.get('/', (req, res) => {
-//     res.render("home", {
-//         user: req.user
-//     })
-// })
-app.get('/', (req, res) => {
-    console.log('req.user:', req.user);
+app.get('/', async(req, res) => {
+    const allBlogs = await Blog.find({});
     res.render("home", {
-        user: req.user
+        user: req.user,
+        blogs: allBlogs
     })
 })
 
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
